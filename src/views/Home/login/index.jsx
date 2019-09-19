eef03f25c11d605fa525cc0b9dfff7f9da4087d5
LHY <lhy136890@163.com>
2019-09-19 21:30:52 +0800

    student

--- src/views/Home/login/index.jsx
+++ src/views/Home/login/index.jsx
@@ -2,24 +2,27 @@
  *   登录
  */
 
- import React, { Component } from 'react'
- 
- class Login extends Component {
-     render() {
-         return (
-             <div className="login"> 
-                <input type="text" placeholder="请输入用户名"/>
-                <input type="text" placeholder="请输入密码"/>
+import React, { Component } from 'react'
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="login">
+                <li>
+                    <input type="text" placeholder="请输入用户名" />
+                </li>
+                <li>
+                    <input type="text" placeholder="请输入密码" />
+                </li>
                 <p>
-                    <input type="checkbox" id=""/>
+                    <input type="checkbox" id="" />
                     <span>两周内自动登录</span>
                 </p>
                 <li>
                     <button>登录</button>
                 </li>
-             </div>
-         )
-     }
- }
- export default Login
- 
+            </div>
+        )
+    }
+}
+export default Login
--- src/views/Home/register/index.jsx
+++ src/views/Home/register/index.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 
- class Register extends Component {
+class Register extends Component {
     render() {
         return (
-            <div className="register"> 
-                <input type="text" placeholder="请输入用户名"/>
-                <input type="text" placeholder="请输入密码"/>
-                
+            <div className="register">
+                <li>
+                    <input type="text" placeholder="请输入用户名" />
+                </li>
+                <li>
+                    <input type="text" placeholder="请输入密码" />
+                </li>
+
                 <li>
                     <button>注册</button>
                 </li>
-             </div>
+            </div>
         )
     }
 }
